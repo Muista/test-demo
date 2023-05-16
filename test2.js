98f5c97d80958a1dce97b252c13fd8e4403bf10e
Muista <578557374@qq.com>
2023-05-16 11:12:57 +0800

    22222

--- test2.js
+++ test2.js
@@ -1,6 +1,6 @@
 windows.onload=function(){
     const oBox=document.querySelector('.box')
     oBox.addEventListener('click',function(){
-        alert('测试git上传github')
+        alert('测试git上传github222222222222222222222222')
     })
 }
